8d7562abc06e8255c9414edf83a5dbff32b8fd3d
Nick Esselman <nick.esselman@gmail.com>
2025-09-15 10:51:51 +0200

    AAAGGHHH!! 02

---
+++ M5ProgDocker/M5Prog/pokemon.sql
@@ -0,0 +1,2 @@
+INSERT INTO `m5prog`.`pokemon` (`id`, `name`, `element`) VALUES ('1', 'buglet', 'earth');
+INSERT INTO `m5prog`.`pokemon` (`id`, `name`, `element`) VALUES ('2', 'kerby', 'cuteness');
---
+++ M5ProgDocker/M5Prog/sortpokemon.sql
@@ -0,0 +1,2 @@
+SELECT * FROM m5prog.pokemon where name = 'kerby';
+SELECT * FROM m5prog.pokemon order by element;
