3358c0b8823f697ca9ad5c4427dda604afb234c0
Nick Esselman <nick.esselman@gmail.com>
2025-09-29 10:37:18 +0200

    AAGHH

---
+++ Db opdrachten/05/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO `bordspellen`(`naam`, `aantalspelers`, `beschijfving`, `coop`) VALUES 
+('Catan','4','Handel grondstoffen en bouw dorpen','0'),
+('Pandemic','4','Werk samen om ziektes te bestrijden','1'),
+('Ticket to Ride','5','Verzamel treinkaarten en bouw routes','0'),
+('Gloomhaven','4','Verken kerkers en versla monsters','1'),
+('Carcassonne','5','Bouw steden en wegen met tegels','0'),
+('Arkham Horror','6','Samen vechten tegen kosmische monsters','1'),
+('Azul','4','Plaats kleurrijke tegels om patronen te maken','0'),
+('Forbidden Island','4','Werk samen om schatten te redden','1'),
+('Splendor','4','Verzamel edelstenen en ontwikkel handel','0'),
+('Spirit Island','4','Bescherm het eiland tegen indringers','1');
